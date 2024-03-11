81c5bc202019d9bee805626aa944ffc13bd2c935
cerentendar <cerentendar@gmail.com>
2024-03-11 21:49:56 +0300

    use strict

---
+++ assigments/class-notes/js19_modules/1-without-modules/calculate.js
@@ -0,0 +1,13 @@
+"use strict" //! Derleyici direktifi
+
+console.log("CALCULATE.JS")
+
+const totalPrice = (products) => {
+  return products.reduce((sum, product) => sum + product.price, 0)
+}
+
+//? Global namespace
+// const students = ["Canan", "Can"]
+
+myVariable = "my string" //! use Strict -> myVariable is not defined
+console.log(myVariable)
